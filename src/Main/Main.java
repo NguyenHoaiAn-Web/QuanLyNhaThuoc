2b602ac4c6a735e96297ab2bfda391b98a343c0e
nguyennhathaicomputer <nguyennhathaigithub@gmail.com>
2025-01-10 18:09:13 +0700

    first commit

---
+++ src/Main/Main.java
@@ -0,0 +1,20 @@
+package Main;
+
+import javax.swing.SwingUtilities;
+
+import GUI.LoginForm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	                new LoginForm().setVisible(true);
+	            }
+	        }
+	        );
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module QuanLyNhaThuoc {
+	requires java.desktop;
+	requires java.logging;
+}
